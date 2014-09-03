0b0488cce770dbec89f4bb789c2767da4b7366ca
Ashley Ansell <ashley.ansell@gmail.com>
2014-09-02 23:46:38 -0400

    Initial Commit

---
+++ NailsiOS/NailsiOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NailsiOSAppDelegate.h
+//  NailsiOS
+//
+//  Created by Ashley Ansell on 9/2/14.
+//  Copyright (c) 2014 Ashley Ansell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NailsiOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NailsiOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NailsiOS
+//
+//  Created by Ashley Ansell on 9/2/14.
+//  Copyright (c) 2014 Ashley Ansell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NailsiOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NailsiOSAppDelegate class]));
+    }
+}
